9586d872436b345895648ec214933ef9fa9599c2
vksthomas <vksthomas@gmail.com>
2019-01-25 03:12:26 +0530

    final

---
+++ Js/home.js
@@ -0,0 +1,9 @@
+// (() => {
+//    var y = document.getElementsByTagNameNS()
+//    function color_change(){
+//      var x = document.querySelectorAll(".four_color_change");
+//            x.forEach(element => {
+//             element.style.color = 'orange'
+//             });
+//         }
+// })()
