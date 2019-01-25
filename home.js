bd9cb1e10b782088bd8b7014d651260caa11ddb5
Vikas Sahu <37984860+vksthomas@users.noreply.github.com>
2019-01-25 10:25:27 +0530

    Delete home.js

--- home.js
+++
@@ -1,9 +0,0 @@
-(() => {
-   var y = document.getElementsByTagNameNS()
-   function color_change(){
-     var x = document.querySelectorAll(".four_color_change");
-           x.forEach(element => {
-            element.style.color = 'orange'
-            });
-        }
-})()
